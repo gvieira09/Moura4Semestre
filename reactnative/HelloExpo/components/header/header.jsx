e2c55d04cd5253d04e2fa6dff832bd6e56d7b171
GUILHERME VIEIRA CARVALHO <44840646856@info.local>
2026-08-05 16:22:10 -0300

    codigo modificado

---
+++ reactnative/HelloExpo/components/header/header.jsx
@@ -0,0 +1,13 @@
+const { View, Text } = require("react-native-web")
+
+
+function Header() {
+    return(
+    <View>
+        <Text>Cabeçalho</Text>
+
+    </View>
+    );
+}
+
+export default Header
